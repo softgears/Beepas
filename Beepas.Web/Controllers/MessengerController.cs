0b9d0dd48ca4f67b7b9aa59ded48ca75813d369c
Yuri Korshev <ykorshev@softgears.ru>
2013-05-13 17:00:22 +1000

    Начало реализации формы чата.

---
+++ Beepas.Web/Controllers/MessengerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Beepas.Web.Classes.Managers;
+using Beepas.Web.Classes.Routing;
+
+namespace Beepas.Web.Controllers
+{
+    /// <summary>
+    /// Контроллер управления чатом
+    /// </summary>
+    public class MessengerController : BaseController
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Web.Mvc.Controller"/> class.
+        /// </summary>
+        public MessengerController(UsersManager usersManager) : base(usersManager)
+        {
+        }
+
+        /// <summary>
+        /// Отображает чат
+        /// </summary>
+        /// <returns></returns>
+        [Route("chat")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
